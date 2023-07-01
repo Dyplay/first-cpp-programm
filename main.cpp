442aa35d419621458c86c58cb658dda428d40e2c
Dyplay <71791638+Dyplay@users.noreply.github.com>
2023-07-01 18:50:38 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//listings/03/listing02.cpp
+
+int main() {
+    char ch1 = 'A';
+    char ch2{'B'};
+    char ch3 = 67;
+    char ch4{68};
+    std::cout << ch1 << ch2 << ch3 << ch4 << "\n";
+    return EXIT_SUCCESS;
+}
