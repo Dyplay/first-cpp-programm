08f098ffe628ee71bec2a108a594947b3df58cd9
Dyplay <71791638+Dyplay@users.noreply.github.com>
2023-07-01 18:57:27 +0200

    +2 cpp programms (number.cpp, stop.cpp)

---
+++ number.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+int main() {
+    int number = 0;
+    std::cout << "Please enter a number: ";
+    std::cin >> number;
+    std::cout << "You have typed the number: " << number
+    return EXIT_SUCCESS;
+}
---
+++ stop.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+int main() {
+    std::cout << "The programm has been started\n";
+    system("PAUSE");
+    return 0;
+}
